f11dcab391e517d01db40b10bff0379a201e95c3
Denis Romanov <denis.romanow@gmail.com>
2022-08-04 21:03:35 +0300

    Lesson #2

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import axios from 'axios'
+import AuthorList from './components/AuthorList.js'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            'authors': []
+        }
+    }
+
+    componentDidMount() {
+        axios
+            .get('http://127.0.0.1:8002/api/authors/')
+            .then(response => {
+                const authors = response.data
+                this.setState(
+                    {
+                        'authors': authors
+                    }
+                )
+            })
+            .catch(error => console.log(error))
+    }
+
+    render() {
+        return (
+            <div>
+                <AuthorList authors={this.state.authors} />
+            </div>
+        )
+    }
+}
+
+export default App;
